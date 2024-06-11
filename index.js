3d91e228f911b6ef93397571d371e4eee7353733
Oleksii_Andriushchenko <oa@readable.games>
2024-06-11 18:55:46 +0300

    fixed sxript

--- index.js
+++ index.js
@@ -1,20 +1,17 @@
 const display = document.getElementById("display");
 
-function clearDisplay(){
-display.value = "";
-};
-
-
-function appendToDisplay(input){
-display.value += input;
+function clearDisplay() {
+    display.value = "";
 }
 
+function appendToDisplay(input) {
+    display.value += input;
+}
 
-function calculate(){
+function calculate() {
     try {
-        display.value = eval(display.value)
-        
+        display.value = eval(display.value);
     } catch (error) {
-        window.alert("Error, press 'C' and try again")
+        window.alert("Error, press 'C' and try again");
     }
 }
